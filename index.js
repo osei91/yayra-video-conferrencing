d6163445332383385ef38d65a1bb349a39da90f8
osei91 <wilberrichy@gmail.com>
2020-11-10 06:22:44 +0000

    fixes to api request

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ SocketHandler(io)
 
 app.use(cors())
 app.use(bodyParser.json())
-
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT,()=>{
